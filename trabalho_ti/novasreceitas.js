346f2e89d11842903e919d905be77c86d0ed1e42
danielcbz <162482141+danielcbz@users.noreply.github.com>
2024-06-10 16:06:48 -0300

    Add files via upload

--- trabalho_ti/novasreceitas.js
+++ trabalho_ti/novasreceitas.js
@@ -1,73 +1,69 @@
 const receitasContainer = document.querySelector(".receitas")
+const formNovaReceita = document.getElementById("novaReceita")
 const urlFetch = "https://d2c501fa-4177-4d7b-a69b-81eb77e71b05-00-1wjvhqjrblfl5.kirk.replit.dev/receitas"
+
+let ingredientes = []
+let filtros = []
+let instrucoes = []
+
+var nome = document.querySelector(".nome");
+
+var tempo = document.querySelector(".tempo");
+
+var rendimento = document.querySelector(".rendimento");
+
+var dificuldade = document.querySelector(".dificuldade");
+
+function adicionarBarra() {
+  var ingrediente = document.querySelector(".ingd");
+  var quantidade = document.querySelector(".qtd");
+  const item = {
+    ingrediente: ingrediente.value,
+    quantidade: quantidade.value
+  }
+  ingredientes.push(item)
+  ingrediente.value = ""
+  quantidade.value = ""
+}
+function addfiltros() {
+  var filtro = document.querySelector(".filtro");
+  filtros.push(filtro.value)
+  filtro.value = ""
+}
+function addmodo_preparo() {
+  var instrucao = document.querySelector(".modo_preparo");
+  instrucoes.push(instrucao.value)
+  instrucao.value = ""
+}
 let receitas = []
 
-const loadData = async ()  => {
-  const response = await fetch(urlFetch)
-  const data = await response.json()
-
-  receitas = data.filter(receita => {
-    if(receita.filtro) {
-      return receita.filtro.includes(filtro)
-    }
-  })
+formNovaReceita.onsubmit = async (e) => {
+  e.preventDefault()
   const update = {
-    title: 'A blog post by Kingsley',
-    body: 'Brilliant post on fetch API',
-    userId: 1,
-    };
-    
-    const options = {
+    nome : nome.value,
+    tempo: tempo.value,
+    rendimento: rendimento.value,
+    dificuldade: dificuldade.value,
+    filtro: filtros,
+    ingredientes,
+    modo_preparo: instrucoes,
+  };
+  const options = {
     method: 'POST',
     headers: {
-    'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(update),
-    };
-    fetch('https://d2c501fa-4177-4d7b-a69b-81eb77e71b05-00-1wjvhqjrblfl5.kirk.replit.dev/receitas', options)
-  .then(data => {
+  };
+  fetch('https://d2c501fa-4177-4d7b-a69b-81eb77e71b05-00-1wjvhqjrblfl5.kirk.replit.dev/receitas', options)
+    .then(data => {
       if (!data.ok) {
         throw Error(data.status);
-       }
-       return data.json();
-      }).then(update => {
+      }
+      return data.json();
+    }).then(update => {
       console.log(update);
-      // {
-      //
-      title: 'A blog post by Kingsley',
-      //
-      body: 'Brilliant post on fetch API',
-      //
-      userId: 1,
-      //
-      id: 101
-      // };
-      }).catch(e => {
+    }).catch(e => {
       console.log(e);
-      });
-
-  receitas.forEach(receita => {
-    receitasContainer.innerHTML += `
-    </div>
-    <script src="novasreceitas.js"></script>
-    <div class="search-container">
-      <input type="text" id="search-input" placeholder="Nome da nova receita...">
-    </div>
-  </div>
-  <script src="novasreceitas.js"></script>
-  <div class="search-container">
-    <input type="text" id="search-input" placeholder="igredientes da nova receita...">
-  </div>
-  </div>
-  <script src="novasreceitas.js"></script>
-  <div class="search-container">
-    <input type="text" id="search-input" placeholder="metodo de preparo da nova receita...">
-  </div>
-  <div class="button-container">
-      <button type="button">Botão Centralizado</button>
-    </div>
-    `
-  })
+    });
 }
-
-window.onload = loadData
